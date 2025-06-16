25cc573105365f10bef2256a77059d4bdff3e469
DM`s Reposit <30522067+GenbuKaito@users.noreply.github.com>
2025-06-16 20:24:31 +0900

    CommentRepository 구현

---
+++ src/main/java/com/example/outsourcing/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.example.outsourcing.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import  com.example.outsourcing.entity.CommentEntity;
+import com.example.outsourcing.entity.TaskEntity;
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<CommentEntity,Long> {
+    List<CommentEntity> findByTaskAndDeletedFalseOrderByCreatedAtDesc(TaskEntity task); /*taskId에 딸린 댓글 검색*/
+    List<CommentEntity> findByContentContainingAndDeletedFalse(String content); /*내용에 대한 Like 검색*/
+}
