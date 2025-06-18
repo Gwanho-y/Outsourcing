3841796f51ffd4d0025a52a5d26dbf3110536cc1
YeJin420 <lyjin030396@gmail.com>
2025-06-18 09:31:19 +0900

    GlobalExceptionHandler 예외 변수명 직관적으로 표기

--- src/main/java/com/example/outsourcing/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/outsourcing/global/exception/GlobalExceptionHandler.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     // Task 관련 예외 처리
     @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException ex) {
+    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException taskNotFoundException) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body("해당 태스크를 찾을 수 없습니다: " + ex.getMessage());
+                .body("해당 태스크를 찾을 수 없습니다: " + taskNotFoundException.getMessage());
     }
 
     // 이후 다른 도메인의 예외들도 여기에 추가
