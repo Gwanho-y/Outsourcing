384f4383013a3435ca569d90daa2c14f004888ab
YoungJae1118 <apricity1118@gmail.com>
2025-06-18 12:02:28 +0900

    유저 id 어떻게 해야 될 지 모르겠음

--- src/main/java/com/example/outsourcing/entity/UserEntity.java
+++ src/main/java/com/example/outsourcing/entity/UserEntity.java
@@ -10,7 +10,8 @@ public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
 
     @Column(nullable = false, length = 255)
     private String email;
