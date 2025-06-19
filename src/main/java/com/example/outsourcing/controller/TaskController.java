e1fb719be18ac8114800f2e34765b59c21dbc05f
YoungJae1118 <apricity1118@gmail.com>
2025-06-19 16:05:28 +0900

    대쉬보드까지 완료

--- src/main/java/com/example/outsourcing/controller/TaskController.java
+++ src/main/java/com/example/outsourcing/controller/TaskController.java
@@ -71,10 +71,6 @@ public class TaskController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 204
     }
 
-
-
-
-
     //대시보드 조회
     @GetMapping("/dashboard")
     public List<DashboardDto> getDashboardStats() {
